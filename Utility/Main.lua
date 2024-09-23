80cf31c68f870209293d59f2ebffac471a1bdc2e
AruissiRAR <162387537+AruissiRAR@users.noreply.github.com>
2024-09-23 19:00:36 -0400

    Update Main.lua

--- Utility/Main.lua
+++ Utility/Main.lua
@@ -1,7 +1,8 @@
 local Utility = { Connections = {}, Old_Functions = {} }
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
-local DebrisService = game:GetService("Debris")
+local DebrisService = game:GetService('Debris');
+local TweenService = game:GetService('TweenService');
 Utility.Client = getsenv(LocalPlayer.PlayerGui.Client)
 
 function Utility:Create_Beam(From: Vector3?|CFrame?, To: Vector3?|CFrame?, Lifetime: number, Transparency: number, Color: Color3?, Thickness: number?, Texture: string?|number?, LightEmission: number?, FaceCamera: boolean?): Beam?
@@ -80,4 +81,107 @@ function Utility:Connect(Signal, Func)
     
     return table.insert(Utility.Connections, Connection)
 end
+
+
+-- // Notification System
+
+local Notfication = Utility:Create_Instance("ScreenGui", {
+    Name = "Notification",
+    Parent = game.Players.LocalPlayer.PlayerGui,
+    ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+})
+
+local NotificationHolder = Utility:Create_Instance("Frame", {
+    Name = "NotificationHolder",
+    Position = UDim2.new(0.018, 0.000, 0.092, 0.000),
+    Size = UDim2.new(0.000, 219.000, 0.000, 629.000),
+    Parent = Notfication,
+    BackgroundTransparency = 1,
+    BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+    BorderSizePixel = 0,
+    ZIndex = 1
+})
+
+Utility:Create_Instance("UIListLayout", {
+    Parent = NotificationHolder,
+    Padding = UDim.new(0, 6),
+    FillDirection = Enum.FillDirection.Vertical,
+    HorizontalAlignment = Enum.HorizontalAlignment.Left,
+    VerticalAlignment = Enum.VerticalAlignment.Top,
+    SortOrder = Enum.SortOrder.LayoutOrder
+})
+
+
+
+function Create_Notification(Text: string, Duration: number)
+	local New_Notification = Utility:Create_Instance("Frame", {
+		Name = "NotificationSample",
+		Position = UDim2.new(0.000, 0.000, 0.000, 0.000),
+		Size = UDim2.new(0.000, 204.000, 0.000, 46.000),
+		Parent = NotificationHolder,
+		BackgroundColor3 = Color3.fromRGB(14, 14, 14),
+		BorderSizePixel = 0,
+		ZIndex = 1
+	})
+
+	Utility:Create_Instance("UIStroke", {
+		Parent = New_Notification,
+		Color = Color3.fromRGB(26, 26, 26),
+		Thickness = 1.600000023841858,
+		Transparency = 0
+	})
+
+	local NotificationFrame = Utility:Create_Instance("Frame", {
+		Name = "NotificationFrame",
+		Position = UDim2.new(0.020, 0.000, 0.087, 0.000),
+		Size = UDim2.new(0.000, 196.000, 0.000, 38.000),
+		Parent = New_Notification,
+		BackgroundColor3 = Color3.fromRGB(21, 21, 21),
+		BorderSizePixel = 0,
+		ZIndex = 1
+	})
+
+	Utility:Create_Instance("UIStroke", {
+		Parent = NotificationFrame,
+		Color = Color3.fromRGB(21, 21, 21),
+		Thickness = 1,
+		Transparency = 0
+	})
+
+	Utility:Create_Instance("TextLabel", {
+		Name = "TextLabel",
+		Position = UDim2.new(-0.020, 0.000, 0.105, 0.000),
+		Size = UDim2.new(0.000, 200.000, 0.000, 30.000),
+		Parent = NotificationFrame,
+		BackgroundTransparency = 1,
+		Font = Enum.Font.SourceSans,
+		Text = Text,
+		TextXAlignment = Enum.TextXAlignment.Center,
+		TextYAlignment = Enum.TextYAlignment.Center,
+		TextColor3 = Color3.fromRGB(178, 178, 178),
+		BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+		TextSize = 14,
+		BorderSizePixel = 0,
+		ZIndex = 1
+	})
+
+	local Timer = Utility:Create_Instance("Frame", {
+		Name = "Timer",
+		Position = UDim2.new(0.000, 0.000, 1.000, 0.000),
+		Size = UDim2.new(0.000, 204.000, 0.000, 1.000),
+		Parent = New_Notification,
+		BackgroundColor3 = Color3.fromRGB(79, 116, 171),
+		BorderSizePixel = 0,
+		ZIndex = 1
+	})
+
+	local TimerTween = TweenService:Create(Timer, TweenInfo.new(Duration, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {Size = UDim2.new(0, 0, 0, 1)})
+	TimerTween:Play()
+
+
+	TimerTween.Completed:Connect(function()	
+		New_Notification:Destroy()
+	end)
+end
+
 return Utility;
