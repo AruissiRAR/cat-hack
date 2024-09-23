57f9389ff83f344d4d7554f207a85e37bbae1b99
AruissiRAR <162387537+AruissiRAR@users.noreply.github.com>
2024-09-23 13:16:28 -0400

    Update Main.lua

--- Utility/Main.lua
+++ Utility/Main.lua
@@ -4,48 +4,50 @@ local LocalPlayer = game:GetService("Players").LocalPlayer
 Utility.Client = getsenv(LocalPlayer.PlayerGui.Client)
 
 function Utility:Create_Beam(From: Vector3?|CFrame?, To: Vector3?|CFrame?, Lifetime: number, Transparency: number, Color: Color3?, Thickness: number?, Texture: string?|number?, LightEmission: number?, FaceCamera: boolean?): Beam?
-    Color = ColorSequence.new(Color) or ColorSequence.new(Color3.fromRGB(255, 255, 255))
-
-
-    local Part_1 = Instance.new("Part", workspace.Ray_Ignore)
-    local Part_2 = Instance.new("Part", workspace.Ray_Ignore)
-
-    local Beam = Instance.new("Beam", Part_1)
-
-    local A1 = Instance.new("Attachment", Part_1)
-    local A2 = Instance.new("Attachment", Part_2)
-
-    Part_1.Transparency = 1
-    Part_2.Transparency = 1
-
-    Part_1.Position = typeof(From) == "Vector3" and From or From.Position
-    Part_2.Position = typeof(To) == "Vector3" and To or To.Position
-
-    Part_1.CanCollide = false
-    Part_2.CanCollide = false
-
-    Part_1.Anchored = true
-    Part_2.Anchored = true
-
-    Beam.Color = Color
-    Beam.Texture = typeof(Texture) == "string" and Texture or "rbxassetid://".. tostring(Texture)
-    Beam.LightEmission = LightEmission
-    Beam.LightInfluence = 0
-    Beam.FaceCamera = FaceCamera
-    Beam.Transparency = NumberSequence.new(Transparency)
-    Beam.Width0 = Thickness
-    Beam.Width1 = Thickness
-    Beam.Attachment0 = A1
-    Beam.Attachment1 = A2
-
     coroutine.wrap(function()
-        for i=Transparency, 1.05, 0.05 do task.wait()
-            Beam.Transparency = NumberSequence.new(i)
-        end
-
-        Part_1:Destroy()
-        Part_2:Destroy()
-    end)
+        Color = ColorSequence.new(Color) or ColorSequence.new(Color3.fromRGB(255, 255, 255))
+    
+    
+        local Part_1 = Instance.new("Part", workspace.Ray_Ignore)
+        local Part_2 = Instance.new("Part", workspace.Ray_Ignore)
+    
+        local Beam = Instance.new("Beam", Part_1)
+    
+        local A1 = Instance.new("Attachment", Part_1)
+        local A2 = Instance.new("Attachment", Part_2)
+    
+        Part_1.Transparency = 1
+        Part_2.Transparency = 1
+    
+        Part_1.Position = typeof(From) == "Vector3" and From or From.Position
+        Part_2.Position = typeof(To) == "Vector3" and To or To.Position
+    
+        Part_1.CanCollide = false
+        Part_2.CanCollide = false
+    
+        Part_1.Anchored = true
+        Part_2.Anchored = true
+    
+        Beam.Color = Color
+        Beam.Texture = typeof(Texture) == "string" and Texture or "rbxassetid://".. tostring(Texture)
+        Beam.LightEmission = LightEmission
+        Beam.LightInfluence = 0
+        Beam.FaceCamera = FaceCamera
+        Beam.Transparency = NumberSequence.new(Transparency)
+        Beam.Width0 = Thickness
+        Beam.Width1 = Thickness
+        Beam.Attachment0 = A1
+        Beam.Attachment1 = A2
+    
+        coroutine.wrap(function()
+            for i=Transparency, 1.05, 0.05 do task.wait()
+                Beam.Transparency = NumberSequence.new(i)
+            end
+    
+            Part_1:Destroy()
+            Part_2:Destroy()
+        end)()
+    end)()
 end
 
 function Utility:Is_Alive(Player: Player): Instance
